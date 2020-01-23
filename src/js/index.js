90ed90e3272b2b9c2633c55b6d0db22649cb501d
tsalinas945 <tsalinas2jz@GMAIL.COM>
2020-01-23 22:33:01 +0000

    domain-generator

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,22 @@ import "../style/index.scss";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  let domain = [];
+
+  for (let p in pronoun)
+    for (let a in adj)
+      for (let n in noun) domain.push(pronoun[p] + adj[a] + noun[n] + ".com");
+
+  let div = document.querySelector(".domain_generator");
+
+  for (let i in domain) {
+    let node = document.createElement("p");
+    let text = document.createTextNode(domain[i]);
+    node.appendChild(text);
+    div.appendChild(node);
+  }
 };
